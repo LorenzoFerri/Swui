793b3997207fc58f579f4696798f30757f03b62c
Lorenzo Ferri <lorenzo.ferri96+github@gmail.com>
2024-04-13 02:31:52 +0200

    Fix memory leak

--- Sources/UIElements/Border.swift
+++ Sources/UIElements/Border.swift
@@ -1,25 +1,27 @@
 import Observation
 import UWP
 import WinUI
+import Foundation
 
 struct Border<Content: Element>: UIElementRepresentable {
     var element: WinUI.Border?
-    var content: () -> Content
-    var borderThickness: () -> Thickness
-    var backgroundColor: () -> Color
+    let content: Content
+    let borderThickness: () -> Thickness
+    let backgroundColor: () -> Color
 
     init(
         borderThickness: @autoclosure @escaping () -> Thickness = .init(),
         backgroundColor: @autoclosure @escaping () -> Color = .transparent,
         @ElementBuilder _ content: @escaping () -> Content
     ) {
-        self.content = content
+        self.content = content()
         self.borderThickness = borderThickness
         self.backgroundColor = backgroundColor
     }
 
     mutating func makeUIElement() -> WinUI.Border? {
         element = WinUI.Border()
+        element?.child = content.makeElement()
         updateUIElement()
         return element
     }
@@ -27,7 +29,6 @@ struct Border<Content: Element>: UIElementRepresentable {
     func updateUIElement() {
         if let element {
             withObservationTracking {
-                element.child = content().makeElement()
                 element.borderThickness = borderThickness()
                 element.background = SolidColorBrush(backgroundColor())
             } onChange: {
